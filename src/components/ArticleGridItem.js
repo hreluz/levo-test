bfd404ec1311af67be97a8a97036be2428480db9
Hector <hmreluz@gmail.com>
2020-12-15 19:54:39 +1100

    Adding logic to make title and paragraph short in ArticleGridItem Component

--- src/components/ArticleGridItem.js
+++ src/components/ArticleGridItem.js
@@ -1,16 +1,20 @@
 import PropTypes from 'prop-types';
 
 const ArticleGridItem = ({date, title, paragraph}) => {
+
+    const titleSubSstring = title.length > 40 && `${ title.substring(0, 40)} ...` ;
+    const paragraphSubSstring = paragraph.length > 300 && `${ paragraph.substring(0, 300)} ...` ;
+
     return (
         <>
-        <div className="card bg-primary text-white text-center p-3">
+        <div className="card bg-primary text-white text-left p-3">
             <small className="text-left">{ date }</small>
             <blockquote className="blockquote mb-0">
-                <p>{title}</p>
+                <p>{titleSubSstring}</p>
                 <div className="blockquote-footer text-white">
-                    <small>{paragraph}</small>
+                    <small>{paragraphSubSstring}</small>
                 </div>
-                <button type="button" className="btn btn-outline-light mt-2">Read More</button>
+                <button type="button" className="btn btn-outline-light mt-4">Read More</button>
             </blockquote>
         </div>
         </>
